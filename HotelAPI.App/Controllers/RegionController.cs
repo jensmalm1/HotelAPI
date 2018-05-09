150534a53e6929e4a5929a414472b29594013613
Jimmy Elwing <accounts@jimmyelwing.se>
2018-05-09 11:39:42 +0200

    Adding a region through api is implemented and works

--- HotelAPI.App/Controllers/RegionController.cs
+++ HotelAPI.App/Controllers/RegionController.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using HotelAPI.Data;
+using HotelAPI.Domain;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
@@ -11,5 +13,70 @@ namespace HotelAPI.App.Controllers
     [Route("api/Region")]
     public class RegionController : Controller
     {
+        private readonly RegionDbManager _regionDbManager = new RegionDbManager();
+
+        [HttpPost]
+        public void AddRegion(Region region)
+        {
+            //var temp = new Region();
+            //temp.Name = name;
+            //temp.Id = id;
+            _regionDbManager.CreateRegion(region);
+        }
+
+        [HttpGet]
+        public void GetAllRegion()
+        {
+
+        }
+
+        [HttpGet("{id}")]
+        public void GetRegion(int id)
+        {
+
+        }
+
+        [HttpDelete ("{id}")]
+        public void DeleteRegion(int id)
+        {
+
+        }
+
+        [HttpPost("recreate")]
+        public void SeedDatabase()
+        {
+
+        }
+
+        public void RecreateDatabase()
+        {
+            
+        }
+
+        public List<Region> ReturnBasicRegions()
+        {
+            var regions = new List<Region>();
+
+            regions.Add(new Region()
+            {
+                Id = 50,
+                Name = "Göteborg Centrum",
+            });
+
+            regions.Add(new Region()
+            {
+                Id = 60,
+                Name = "Göteborg Centrum",
+            });
+
+            regions.Add(new Region()
+            {
+                Id = 70,
+                Name = "Helsingborg",
+            });
+
+            return regions;
+        }
+
     }
 }
---
+++ HotelAPI.Data/RegionDbManager.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HotelAPI.Domain;
+
+namespace HotelAPI.Data
+{
+    public class RegionDbManager : IDisposable
+    {
+        private readonly HotelContext _context = new HotelContext();
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void CreateRegion(Region region)
+        {
+            _context.Add(region);
+            _context.SaveChanges();
+        }
+    }
+}
