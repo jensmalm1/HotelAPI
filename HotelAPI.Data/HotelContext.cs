bd30ebcb122be35365f0e51f85117cc5c6172ee4
Jimmy Elwing <accounts@jimmyelwing.se>
2018-05-09 10:28:56 +0200

    EntityFramework is installed and working

--- HotelAPI.Data/HotelContext.cs
+++ HotelAPI.Data/HotelContext.cs
@@ -9,18 +9,16 @@ namespace HotelAPI.Data
 
         public DbSet<Region> Regions { get; set; }
 
+
+        //protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        //{
+        //    base.OnConfiguring(optionsBuilder);
+        //}
+
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             optionsBuilder.UseSqlServer(
                 "Server = (localdb)\\mssqllocaldb; Database = EfHotelApi; Trusted_Connection = True; ");
-        }
-    }
-
-    public class Program
-    {
-        static void Main(string[] args)
-        {
-
         }
     }
 }
