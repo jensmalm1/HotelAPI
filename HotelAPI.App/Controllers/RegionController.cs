8cfcc9e8742683032bcc1b9df191c2e51a161fe9
JensMalm <jensmalm873@gmail.com>
2018-05-09 10:38:10 +0200

    Region.js och RegionController tillagd

---
+++ HotelAPI.App/Controllers/RegionController.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HotelAPI.App.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/Region")]
+    public class RegionController : Controller
+    {
+    }
+}
