a2294bd9063688cb2237b28b566570b267786b16
Jimmy Elwing <accounts@jimmyelwing.se>
2018-05-15 17:01:14 +0200

    Parser-test working

--- HotelApi.Domain.Test/HotelAdderTest.cs
+++ HotelApi.Domain.Test/HotelAdderTest.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using HotelAPI.Domain;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Newtonsoft.Json.Linq;
 
 namespace HotelApi.Domain.Test
 {
@@ -39,6 +40,30 @@ namespace HotelApi.Domain.Test
             Assert.AreEqual(expected.RegionValue, actual.RegionValue);
             Assert.AreEqual(expected.Rooms, actual.Rooms);
             Assert.AreEqual(5, hotelList.Count);
+        }
+
+        [TestMethod]
+        public void AddWesternHotelsToHotelList_AddsHotelFromString_HotelAdded()
+        {
+            var hotelAdder = new HotelAdder();
+
+            var westernJsonFile = "[\r\n  {\r\n    \"Reg\": 50,\r\n    \"Name\": \"Hotell Eggers\",\r\n    \"LedigaRum\": 100\r\n  },\r\n  {\r\n    \"Reg\": 50,\r\n    \"Name\": \"Tidbloms Hotel\",\r\n    \"LedigaRum\": 200\r\n  },\r\n  {\r\n    \"Reg\": 70,\r\n    \"Name\": \"Hotell Duxiana\",\r\n    \"LedigaRum\": 5\r\n  }\r\n]";
+
+            var hotelList = new List<Hotel>();
+            hotelAdder.AddWesternHotelsToHotelList(hotelList, westernJsonFile);
+            var expected = new Hotel
+            {
+                Name = "Hotell Eggers",
+                RegionValue = 50,
+                Rooms = 100
+            };
+
+            var actual = hotelList[0];
+
+            Assert.AreEqual(expected.Name, actual.Name);
+            Assert.AreEqual(expected.RegionValue, actual.RegionValue);
+            Assert.AreEqual(expected.Rooms, actual.Rooms);
+            Assert.AreEqual(3, hotelList.Count);
         }
     }
 }
--- HotelApi.Domain.Test/ParserTest.cs
+++ HotelApi.Domain.Test/ParserTest.cs
@@ -11,7 +11,18 @@ namespace HotelApi.Domain.Test
         [TestMethod]
         public void Name()
         {
-            throw new NotImplementedException();
+            var appConfiguration = new AppConfiguration();
+            appConfiguration.ImportPath = "C:\\project\\HotelApi\\HotelApi.Domain.Test";
+            var parser = new Parser(appConfiguration);
+
+            var jsonFiles = parser.SortJsonFilesByDate();
+
+            var actual = jsonFiles[0];
+            var expected = "C:\\project\\HotelApi\\HotelApi.Domain.Test\\BestWestern-2018-05-15.json";
+
+            Assert.AreEqual(expected, actual);
+
+
         }
     }
 }
