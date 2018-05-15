2e631e61ce3d59c88abb626842a23b8b3fece964
JensMalm <jensmalm873@gmail.com>
2018-05-15 13:12:03 +0200

    Init Test

---
+++ Tests/UnitTest1.cs
@@ -0,0 +1,17 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace HotelAPI.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TypeCorrectRegionValue()
+        {
+            var expected = 50;
+            var 
+
+
+        }
+    }
+}
